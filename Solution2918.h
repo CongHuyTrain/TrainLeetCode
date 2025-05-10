ae34900819ed87961c88c3864860254462ef77c3
CongHuyTrain <leconghuy2005x1@gmail.com>
2025-05-10 21:13:10 +0700

    solution2918

---
+++ Solution2918.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+#include<iostream>
+#include<vector>
+using namespace std;
+//2918. Minimum Equal Sum of Two Arrays After Replacing Zeros
+//Solved
+//Medium
+//Topics
+//Companies
+//Hint
+//You are given two arrays nums1 and nums2 consisting of positive integers.
+//You have to replace all the 0's in both arrays with strictly positive integers such that the sum of elements of both arrays becomes equal.
+//Return the minimum equal sum you can obtain, or -1 if it is impossible.
+//Example 1:
+//Input: nums1 = [3, 2, 0, 1, 0], nums2 = [6, 5, 0]
+//Output : 12
+//Explanation : We can replace 0's in the following way:
+//- Replace the two 0's in nums1 with the values 2 and 4. The resulting array is nums1 = [3,2,2,1,4].
+//- Replace the 0 in nums2 with the value 1. The resulting array is nums2 = [6, 5, 1].
+//Both arrays have an equal sum of 12. It can be shown that it is the minimum sum we can obtain.
+//Example 2 :
+//
+//    Input : nums1 = [2, 0, 2, 0], nums2 = [1, 4]
+//    Output : -1
+//    Explanation : It is impossible to make the sum of both arrays equal.
+//    Constraints :
+//    1 <= nums1.length, nums2.length <= 105
+//    0 <= nums1[i], nums2[i] <= 106
+class Solution {
+public:
+    long long minSum(vector<int>& nums1, vector<int>& nums2) {
+        int countZero1 = 0, countZero2 = 0;
+        long long sumNums1 = 0, sumNums2 = 0;
+        // count 0 and sum.
+        for (int i = 0; i < nums1.size(); i++) {
+            if (nums1[i] == 0)   countZero1++;
+            else    sumNums1 += nums1[i];
+        }
+        for (int i = 0; i < nums2.size(); i++) {
+            if (nums2[i] == 0)   countZero2++;
+            else    sumNums2 += nums2[i];
+        }
+        //check if two vector is 0, we return itself if equal.
+        if (countZero1 == 0 && countZero2 == 0) {
+            return sumNums1 == sumNums2 ? sumNums1 : -1;
+        }
+        else if (countZero1 == 0 || countZero2 == 0) { // else if situation that we can not adjust two vector, 1 of them is a constant
+            if (countZero1 == 0) { // if vector one is a constant( hằng số), then if it bigger vector 2 + the minimum of change 0 to positive
+                if (sumNums1 >= sumNums2 + countZero2) return sumNums1;
+                else    return -1;
+            }
+            else {
+                if (sumNums2 >= sumNums1 + countZero1) return sumNums2;
+                else    return -1;
+            }
+        }
+        else return sumNums1 + countZero1 >= sumNums2 + countZero2 ? sumNums1 + countZero1 : sumNums2 + countZero2; // last situation
+    }
+};
