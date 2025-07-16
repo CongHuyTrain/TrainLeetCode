411ae1a9557bfb5ce47fc04a8abaa128d5b46a8a
CongHuyTrain <leconghuy2005x1@gmail.com>
2025-07-16 12:30:23 +0700

    solution 3201 3330

---
+++ Solution3201.h
@@ -0,0 +1,59 @@
+#pragma once
+#include<iostream>
+#include<vector>
+using namespace std;
+//3201. Find the Maximum Length of Valid Subsequence I
+//Solved
+//Medium
+//You are given an integer array nums.
+//A subsequence sub of nums with length x is called valid if it satisfies :
+//(sub[0] + sub[1]) % 2 == (sub[1] + sub[2]) % 2 == ... == (sub[x - 2] + sub[x - 1]) % 2.
+//Return the length of the longest valid subsequence of nums.
+//A subsequence is an array that can be derived from another array 
+//by deleting some or no elements without changing the order of the remaining elements.
+//Example 1:
+//Input: nums = [1, 2, 3, 4]
+//Output : 4
+//Explanation :
+//	The longest valid subsequence is[1, 2, 3, 4].
+//	Example 2 :
+//	Input : nums = [1, 2, 1, 1, 2, 1, 2]
+//	Output : 6
+//	Explanation :
+//	The longest valid subsequence is[1, 2, 1, 2, 1, 2].
+//	Example 3 :
+//	Input : nums = [1, 3]
+//	Output : 2
+//	Explanation :
+//	The longest valid subsequence is[1, 3].
+//Constraints:
+//2 <= nums.length <= 2 * 105
+//1 <= nums[i] <= 107
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int countOdd = 0;
+        int countEven = 0;
+        int countTwo = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 == 0) {
+                countEven++;
+            }
+            else {
+                countOdd++;
+            }
+        }
+        int flat = nums[0] % 2;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] % 2 != flat) {
+                countTwo += 1;
+                if (nums.size() - i >= 2) {
+                    flat = nums[i] % 2;
+                }
+                else    break;
+            }
+            else    continue;
+        }
+        return max(max(countTwo, countEven), countOdd);
+    }
+};
